aaa34875978032e135786893c615f3dcfb07edcb
Mikita Subbot <Mikita_Subbot@epam.com>
2021-03-04 09:51:13 +0300

    task 1.1

---
+++ index.js
@@ -0,0 +1,23 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+    input: process.stdin, output: process.stdout
+});
+
+const TEXT_TEMPLATE = 'Type any text: ';
+const getResult = (result) => `Result: ${result} \n ----------`
+const getReverse = (str) => str.split('').reverse().join('');
+
+rl.question(TEXT_TEMPLATE + '\n', (value) => {
+    const result = getReverse(value)
+    console.log(getResult(result))
+    rl.setPrompt(TEXT_TEMPLATE + '\n')
+    rl.prompt()
+    rl.on('line', (value) => {
+        const result = getReverse(value)
+        console.log(`${getResult(result)}\n${TEXT_TEMPLATE}`)
+    })
+
+})
+
+
