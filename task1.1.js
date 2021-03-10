4715367887e8ffda7f0f7930f915f0ff69e7f52e
Mikita Subbot <Mikita_Subbot@epam.com>
2021-03-10 12:26:07 +0300

    fix tsk1.1

--- task1.1.js
+++ task1.1.js
@@ -1,23 +1,33 @@
-const readline = require('readline')
+function reverseString(data){ 
+    const reverseStr= data.toString().split('').reverse().join('');
+    process.stdout.write(reverseStr.trim() + '\n\n');
+}
 
-const rl = readline.createInterface({
-    input: process.stdin, output: process.stdout
-});
+process.stdin.on('data', reverseString);
 
-const TEXT_TEMPLATE = 'Type any text: ';
-const getResult = (result) => `Result: ${result} \n ----------`
-const getReverse = (str) => str.split('').reverse().join('');
 
-rl.question(TEXT_TEMPLATE + '\n', (value) => {
-    const result = getReverse(value)
-    console.log(getResult(result))
-    rl.setPrompt(TEXT_TEMPLATE + '\n')
-    rl.prompt()
-    rl.on('line', (value) => {
-        const result = getReverse(value)
-        console.log(`${getResult(result)}\n${TEXT_TEMPLATE}`)
-    })
 
-})
+// const readline = require('readline')
+
+// const rl = readline.createInterface({
+//     input: process.stdin, output: process.stdout
+// });
+
+// const TEXT_TEMPLATE = 'Type any text: ';
+// const getResult = (result) => `Result: ${result} \n ----------`
+// const getReverse = (str) => str.split('').reverse().join('');
+
+// rl.question(TEXT_TEMPLATE + '\n', (value) => {
+//     const result = getReverse(value)
+//     console.log(getResult(result))
+//     rl.setPrompt(TEXT_TEMPLATE + '\n')
+//     rl.prompt()
+//     rl.on('line', (value) => {
+//         const result = getReverse(value)
+//         console.log(`${getResult(result)}\n${TEXT_TEMPLATE}`)
+//     })
+
+// })
+
 
 
