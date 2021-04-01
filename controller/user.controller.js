fae99eb4fe22f2280673a32dc3485b077a12601a
Kejno <1patrik@mail.ru>
2021-04-01 15:58:56 +0300

    fix for create and delete users

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -9,15 +9,15 @@ class UserController {
             login,
             password,
             age,
-            isdeleted: false
+            isDeleted: false
         });
+        const newUser = createNewUser(req.body);
 
-        const isLoginExists = users.filter(user => user.login === newUser.login);
+        const isLoginExists = users.filter(user => !user.isDeleted && user.login === newUser.login);
         if (isLoginExists.length) {
             return res.json({ message: 'login should be unique' });
         }
 
-        const newUser = createNewUser(req.body);
 
         users.push(newUser);
 
@@ -42,7 +42,15 @@ class UserController {
 
             const newUsersList = limit ? sortedUsersList.slice(0, limit) : sortedUsersList;
 
-            return { users: newUsersList, count: newUsersList.length };
+            const usersListForResponse = newUsersList
+                .filter(user => !user.isDeleted)
+                .map(user => {
+                    // eslint-disable-next-line no-unused-vars
+                    const { password, isDeleted, ...rest } = user;
+                    return rest;
+                });
+
+            return { users: usersListForResponse, count: usersListForResponse.length };
         };
 
         res.json(getAutoSuggestUsers(loginSubstring, limit));
@@ -70,19 +78,18 @@ class UserController {
     deleteUser(req, res) {
         const id = req.params.id;
         const currentUser = users.filter(user => user.id === id);
-        users = users.filter(user => user.id !== id);
 
-        if (currentUser[0].isdeleted === true) {
+        if (currentUser.length && currentUser[0].isDeleted || !currentUser.length) {
             return res.status(400).json(ApiError.badRequest('user not found'));
         }
+        users = users.map(user => {
+            if (user.id === id) {
+                user.isDeleted = true;
+            }
+            return user;
+        });
 
-        const deletedUser = {
-            ...currentUser[0],
-            isdeleted: true
-        };
-
-        users.push(deletedUser);
-        res.json(deletedUser);
+        res.json({ message: 'successfully deleted' });
     }
 }
 
